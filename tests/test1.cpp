2b8cbb485b54f468d4b8ed4d486f43bc810e1e54
Hal Finkel <hal.finkel.oss@gmail.com>
2023-07-05 01:09:11 -0400

    Add license header to the regression test

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,3 +1,9 @@
+//          Copyright Hal Finkel 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+// SPDX-License-Identifier: BSL-1.0
+
 #include <cardpile.hpp>
 
 #include <array>
